6b9bb294b5810bfe6905e6f014deedebba6500cb
azulamb <1127785+azulamb@users.noreply.github.com>
2025-01-22 20:49:32 +0900

    Add export types.

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import { params } from './src/webview2_params.ts';
 import { Microsoft_Web_WebView2, Microsoft_Windows_ImplementationLibrary } from './src/version.ts';
+export type { WEBVIEW2_FUNCS } from './src/webview2.d.ts';
 
 export function createWebView2(dllPath = 'webview2.dll') {
   return Deno.dlopen(
