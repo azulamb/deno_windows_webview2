6f732c721b655d9e4c2d9e71f9fb50e4cc7711f3
azulamb <1127785+azulamb@users.noreply.github.com>
2025-07-14 19:55:10 +0900

    Fix import.

--- tools/webview2.ts
+++ tools/webview2.ts
@@ -1,18 +1,14 @@
-import release from '../webview2/x64/Release/webview2.dll' with {
-  type: 'bytes',
-};
-import debug from '../webview2/x64/Debug/webview2.dll' with { type: 'bytes' };
-
-export function getDll(isDebug = false): Uint8Array {
-  if (isDebug) {
-    return debug;
-  }
-  return release;
+export function getDll(isDebug = false): Promise<Uint8Array> {
+  return Deno.readFile(
+    `../webview2/x64/${isDebug ? 'Debug' : 'Release'}/webview2.dll`,
+  );
 }
 
 export function createDllFile(
   path: string | URL,
   isDebug = false,
 ): Promise<void> {
-  return Deno.writeFile(path, isDebug ? debug : release);
+  return getDll(isDebug).then((body) => {
+    return Deno.writeFile(path, body);
+  });
 }
